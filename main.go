135ffb8e633490bf4c0fc5c737f878034e90a785
Charlie Root <root@obsd74.my.domain>
2024-02-29 03:56:22 +0800

    misc update

--- main.go
+++ main.go
@@ -4,22 +4,22 @@ import (
 	"arknet/database"
 	"arknet/modules/common/model"
 	"arknet/modules/os/model"
-  "arknet/modules/os/view"
+	"arknet/modules/os/view"
 	"arknet/modules/pf/model"
 	"arknet/modules/pf/view"
-  "arknet/modules/pf/view/web"
+	"arknet/modules/pf/view/web"
+	"arknet/view"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
-  "arknet/view"
 	"log"
-  "github.com/gin-contrib/sessions"
-  "github.com/gin-contrib/sessions/cookie"
 )
 
 func main() {
 	var (
-		gin_ip   = common.GetEnvVariable("GIN_IP")
-		gin_port = common.GetEnvVariable("GIN_PORT")
-    gin_secret = common.GetEnvVariable("GIN_SECRET")
+		gin_ip     = common.GetEnvVariable("GIN_IP")
+		gin_port   = common.GetEnvVariable("GIN_PORT")
+		gin_secret = common.GetEnvVariable("GIN_SECRET")
 	)
 
 	log.Printf("Gin Socket: %s:%s\n", gin_ip, gin_port)
@@ -32,15 +32,15 @@ func main() {
 	pfmodel.MigrateDB(db)
 	osmodel.MigrateDB(db)
 	r := gin.Default()
-  store := cookie.NewStore([]byte(gin_secret))
-  r.Use(sessions.Sessions("arknet", store))
-  r.LoadHTMLGlob("templates/*")
-  r.Static("/assets", "./public/core/assets")
-  r.Static("/js", "./public/core/js")
-  r.Static("/css", "./public/core/css")
-  core.Setroutes(r)
+	store := cookie.NewStore([]byte(gin_secret))
+	r.Use(sessions.Sessions("arknet", store))
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./public/core/assets")
+	r.Static("/js", "./public/core/js")
+	r.Static("/css", "./public/core/css")
+	core.Setroutes(r)
 	pfapi.Setroutes(r, db)
-  pfui.Setroutes(r, db)
-  osapi.Setroutes(r, db)
+	pfui.Setroutes(r, db)
+	osapi.Setroutes(r, db)
 	r.Run(gin_ip + ":" + gin_port)
 }
